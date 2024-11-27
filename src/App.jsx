0841446ee6570578eedfc346e2e47bf99aa0f7b1
Mayank <mayank.rhl2004@gmail.com>
2024-11-28 01:07:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todo from './Components/Todo'
+function App() {
+
+
+    return (
+        <>
+            <AddTodo />
+            <Todo />
+        </>
+    )
+}
+
+export default App
